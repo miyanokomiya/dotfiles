4d303f801c51ba7cd970dd5d30aaa2b36b9f0b36
miyanokomiya <tomoya.hstexze24632@gmail.com>
2018-12-17 21:26:35 +0900

    deinインストール追加

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+if [ ! -d ~/.cache/dein ]; then
+  mkdir -p ~/.cache/dein
+  cd ~/.cache/dein
+  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+  sh ./installer.sh  ~/.cache/dein
+fi
+
 ln -snfv ~/dotfiles/.vimrc ~/.vimrc
 mkdir ~/.cache/dein
 ln -snfv ~/dotfiles/dein/.dein.toml ~/.cache/dein/.dein.toml 
