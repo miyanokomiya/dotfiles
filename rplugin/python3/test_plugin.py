dba88dcaeedd48daf95b80c249fb4f0e69c4634b
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-02-05 23:26:13 +0900

    visual範囲取得

--- rplugin/python3/test_plugin.py
+++ rplugin/python3/test_plugin.py
@@ -12,8 +12,13 @@ class TestPlugin(object):
 
     @neovim.command("TestCommand", range='', nargs='*')
     def testcommand(self, args, range):
-        self.nvim.current.line = ('Command with args: {}, range: {}'
-                                  .format(args, range))
+        #  self.nvim.current.line = ('Command with args: {}, <: {}, >: {}'
+        #          .format(args, self.nvim.eval('getpos("\'<")[1:2]'), self.nvim.eval('getpos("\'>")[1:2]')))
+        #  print(self.nvim.current.range)
+        start_r, start_c = self.nvim.eval('getpos("\'<")[1:2]')
+        end_r, end_c = self.nvim.eval('getpos("\'>")[1:2]')
+        self.nvim.out_write('start_r: {}, start_c: {}, '.format(start_r, start_c))
+        self.nvim.out_write('end_r: {}, end_c: {} \n'.format(end_r, end_c))
 
     @neovim.autocmd('BufEnter', pattern='*.py', eval='expand("<afile>")', sync=True)
     def on_bufenter(self, filename):
