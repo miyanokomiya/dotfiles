5cea6644d35edfb28cd17d33b869d64631a338c9
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-02-07 08:31:26 +0900

    リファクタとコマンド名変更

--- rplugin/python3/base64.py
+++ rplugin/python3/base64.py
@@ -7,15 +7,17 @@ class Base64(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @neovim.command("DecodeBase64", range='', nargs='?')
-    def decodebase64(self, args, range):
-        # visual or 引数でbase64文字列を指定
-        text = self.getVisualText() if len(args) == 0 else args[0]
+    @neovim.command("Base64Decode", range='', nargs='?')
+    def decode(self, args, range):
         try:
-            decoded = base64.b64decode(text).decode('utf-8')
+            decoded = base64.b64decode(self.getText(args)).decode('utf-8')
             self.nvim.out_write(decoded + '\n')
         except Exception as e:
-            self.nvim.out_write('Failed to decode: {}\n'.format(e.args))
+            self.nvim.err_write('Failed to decode: {}\n'.format(e.args))
+
+    def getText(self, args):
+        # visual or 引数でbase64文字列を指定
+        return self.getVisualText() if len(args) == 0 else args[0]
 
     def getVisualText(self):
         start_r, start_c = self.nvim.eval('getpos("\'<")[1:2]')
