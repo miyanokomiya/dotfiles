51fc2afeaf330b01a0b94c704a9a96c3dc9e23eb
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-02-06 23:41:16 +0900

    base64デコードプラグイン作成

--- rplugin/python3/test_plugin.py
+++ rplugin/python3/test_plugin.py
@@ -1,4 +1,5 @@
 import neovim
+import base64
 
 @neovim.plugin
 class TestPlugin(object):
@@ -6,20 +7,29 @@ class TestPlugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @neovim.function("TestFunction", sync=True)
-    def testfunction(self, args):
-        return 3
+    @neovim.command("DecodeBase64", range='', nargs='?')
+    def decodebase64(self, args, range):
+        # visual or 引数でbase64文字列を指定
+        text = self.getVisualText() if len(args) == 0 else args[0]
+        try:
+            decoded = base64.b64decode(text).decode('utf-8')
+            self.nvim.out_write(decoded + '\n')
+        except Exception as e:
+            self.nvim.out_write('Failed to decode: {}\n'.format(e.args))
 
-    @neovim.command("TestCommand", range='', nargs='*')
-    def testcommand(self, args, range):
-        #  self.nvim.current.line = ('Command with args: {}, <: {}, >: {}'
-        #          .format(args, self.nvim.eval('getpos("\'<")[1:2]'), self.nvim.eval('getpos("\'>")[1:2]')))
-        #  print(self.nvim.current.range)
+    def getVisualText(self):
         start_r, start_c = self.nvim.eval('getpos("\'<")[1:2]')
         end_r, end_c = self.nvim.eval('getpos("\'>")[1:2]')
-        self.nvim.out_write('start_r: {}, start_c: {}, '.format(start_r, start_c))
-        self.nvim.out_write('end_r: {}, end_c: {} \n'.format(end_r, end_c))
-
-    @neovim.autocmd('BufEnter', pattern='*.py', eval='expand("<afile>")', sync=True)
-    def on_bufenter(self, filename):
-        self.nvim.out_write("testplugin is in " + filename + "\n")
+        lines = self.nvim.current.buffer.range(start_r, end_r)
+        text = ''
+        for c, line in enumerate(lines):
+            if c == 0:
+                if lines.__len__() == 1:
+                    text += line[start_c - 1:end_c]
+                else:
+                    text += line[start_c - 1:]
+            elif c < lines.__len__() - 1:
+                text += line
+            else:
+                text += line[0:end_c]
+        return text
