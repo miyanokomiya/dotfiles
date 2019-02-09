7a2d774b49c85da1cacc3978ca6a5fae3f651423
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-02-09 22:57:14 +0900

    数値付きテキストのソート

--- rplugin/python3/myplugin/utils/sort_utils.py
+++ rplugin/python3/myplugin/utils/sort_utils.py
@@ -1,12 +1,39 @@
-def sort_csv(text):
+import re
+from functools import cmp_to_key
+
+
+def sort_csv(lines):
     """ CSV文字列のソート """
 
-    splitedList = map(lambda s: s.strip(), text.split(','))
+    splitedList = map(lambda s: s.strip(), lines.split(','))
     sortedList = sorted(splitedList)
     return ', '.join(sortedList)
 
 
-def sort_object(text):
+def sort_object(lines):
     """ Object文字列のソート """
 
-    return text
+    return lines
+
+
+def sort_by_number(lines):
+    """ 数値付きテキストのソート """
+
+    pattern = r'([+-]?[0-9]+\.?[0-9]*)'
+    num_lines = []
+    without_num_lines = []
+    for line in lines:
+        num_str = re.match(pattern, line)
+        if num_str:
+            num_lines.append(
+                    {'num': float(num_str.group(0)), 'line': line})
+        else:
+            without_num_lines.append(line)
+
+    sorted_num_lines = sorted(
+            num_lines, key=cmp_to_key(lambda a, b: a['num'] - b['num']))
+    sorted_with_num_lines = list(
+            map(lambda data: data['line'], sorted_num_lines))
+    sorted_without_num_lines = sorted(without_num_lines)
+
+    return type(lines)(sorted_with_num_lines + sorted_without_num_lines)
--- rplugin/python3/myplugin/utils/test_sort_utils.py
+++ rplugin/python3/myplugin/utils/test_sort_utils.py
@@ -13,6 +13,24 @@ class TestSortModule(TestCase):
             ('c: a, a: c, d: 1, b: 11', 'a: c, b: 11, c: a, d: 1'),  # json型
         ]
 
-        for text, result in test_patterns:
-            with self.subTest(text=text, result=result):
-                self.assertEqual(sort_utils.sort_csv(text), result)
+        for lines, result in test_patterns:
+            with self.subTest(lines=lines, result=result):
+                self.assertEqual(sort_utils.sort_csv(lines), result)
+
+    def test_sort_by_number(self):
+        """ sort_by_numberのテスト """
+
+        test_patterns = [
+            # 基本型
+            (('2c', '1a', '3d', '0b'), ('0b', '1a', '2c', '3d')),
+            # 引数の型を維持
+            (['2c', '1a', '3d', '0b'], ['0b', '1a', '2c', '3d']),
+            # 文字のみは末尾
+            (['2c', 'g', 'f', '0b'], ['0b', '2c', 'f', 'g']),
+            # 小数点あり
+            (('2.1c', '2.0a', '2d', '2.2b'), ('2.0a', '2d', '2.1c', '2.2b')),
+        ]
+
+        for lines, result in test_patterns:
+            with self.subTest(lines=lines, result=result):
+                self.assertEqual(sort_utils.sort_by_number(lines), result)
