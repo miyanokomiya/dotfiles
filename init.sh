3f0c1a2045376e0003f82bfffe1720c22342e9bb
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-03-26 12:19:16 +0900

    fix

--- init.sh
+++ init.sh
@@ -67,7 +67,7 @@ git config --global alias.gr "log --graph --pretty=format:'%Cred%h%Creset -%C(ye
 git config --global alias.gra "log --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git config --global alias.l "log"
 git config --global alias.lo "log --pretty='format:%C(yellow)%h %C(green)%cd %C(reset)%s %C(red)%d %C(cyan)[%an]' --date=iso"
-git config --global alias.pc "pull --rebase origin $(git current-branch)"
+git config --global alias.pc "pull --rebase origin $(git rev-parse --abbrev-ref HEAD)"
 git config --global alias.s "status"
 git config --global alias.bc "!f () { git checkout $1; git branch --merged|egrep -v '\*|develop|master'|xargs git branch -d; };f"
 
